cfdda3f6eab15f71bcc1b1178dab48494d3debe0
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-04 14:27:13 +0700

    membuat endpoint post untuk data tabel kandang

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+})
+    .catch((err)=> {
+        console.log(err);
+    })
+
+app.post('/kandang', async (req, res)=> {
+    const data = req.body;
+    try{
+        const kandang = await db.kandang.create(data);
+        res.send(kandang);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+
+
+
+
