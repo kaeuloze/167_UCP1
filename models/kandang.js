165bd41bd3aede2279d270c1f69c650761d46f14
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-04 14:23:21 +0700

    membuat file kandang.js

--- models/kandang.js
+++ models/kandang.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    const kandang = sequelize.define('komik', {
+    const kandang = sequelize.define('kandang', {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
